87b3bf6be9f1a14131f0c2335b8312d593eeab37
DaviZTS <davitassara@gmail.com>
2023-10-13 00:37:18 -0300

    Teste Davi

---
+++ TesteDavi.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(500, 500), "SFML Funciona! Teste Bola Branca");
+    sf::CircleShape shape(250.f);
+    shape.setFillColor(sf::Color(255, 255, 255));
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
