5f0b828dbfed9b4a55cd41069675df8e36947e6e
DaviZTS <davitassara@gmail.com>
2023-10-14 19:46:23 -0300

    Nova janela aberta com o sprite do Knight

--- TesteDavi.cpp
+++ TesteDavi.cpp
@@ -17,5 +17,24 @@ int main() {
         window.display();
     }
 
+    sf::RenderWindow knightWindow(sf::VideoMode(256, 256), "Teste com sprite do Knight");
+    knightWindow.setVerticalSyncEnabled(true);
+
+    sf::Texture t;
+    t.loadFromFile("Knight64x64.png");
+
+    while (knightWindow.isOpen()) {
+        sf::Event e;
+        while (knightWindow.pollEvent(e)) {
+            if (e.type == sf::Event::Closed) knightWindow.close();
+        }
+
+        sf::Sprite knight(t);
+
+        knightWindow.clear();
+        knightWindow.draw(knight);
+        knightWindow.display();
+    }
+    
     return 0;
 }
