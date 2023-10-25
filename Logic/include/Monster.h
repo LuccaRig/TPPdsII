3f8c86f59d2133a298868dabf94199c464198680
pedro0caversan <pedrocaversanpassos@gmail.com>
2023-10-25 20:58:05 -0300

    Modificação do Monster.h e do Item.h

--- Logic/include/Monster.h
+++ Logic/include/Monster.h
@@ -5,42 +5,42 @@
 
 #include "Board.h"
 #include "Tile.h"
-#include "Itens.h"
-// #include "Hero.h"
+#include "Item.h"
+#include "Hero.h"
 
 class Monster {
   
   public:
 
     /// @brief construtor
-    Monster(int monster_type_);
+    Monster(std::string monster_type_);
 
     /// @brief destrutor
     ~Monster();
     
-    /// @brief movimenta o monstro para uma posição mais próxima de um herói. PRECONDIÇÃO: monstro não está a alcance de ataque
+    /// @brief movimenta o monstro para uma posição mais próxima de um herói. PRECONDIÇÃO: nenhum herói está a alcance de ataque
     void move_this_monster();
 
-    /// @brief ataca o herói. PRECONDIÇÃO: monstro está a alcance de ataque
+    /// @brief ataca o herói. PRECONDIÇÃO: algum herói está a alcance de ataque 
     void attack_hero();
 
     /// @brief determina se um item será deixado pelo monstro e qual.
     void decide_monster_item();
 
-    /// @return retorna 1 se o monstro estiver morto e 0 se não estiver.
+    /// @return retorna true se o monstro estiver morto e false se não estiver.
     bool monster_is_dead();
 
     /// @return retorna o número de experiência que o monstro dará quando for derrotado
     int return_exp_drop();
 
 
-
+    // aqui será construído um item e monster_item_drop_ será igualado à função returns_random_item
   private:
     std::string monster_type_;
     int monster_hp_;
     int monster_dmg_;
     int monster_exp_drop_;
-    int monster_item_drop_;
+    std::string monster_item_drop_;
 
 
 };
